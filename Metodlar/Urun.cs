0423aa5c11befe0e13f4d0edcfc9d15b661c81cb
Dilara <114511795+dilarakocamann@users.noreply.github.com>
2022-11-18 19:26:40 +0300

    Update Urun.cs

--- Metodlar/Urun.cs
+++ Metodlar/Urun.cs
@@ -9,9 +9,9 @@ namespace Metodlar
     class Urun
     {
         public int Id { get; set; }
-        public string? Adi { get; set; }
+        public string Adi { get; set; }
         public double Fiyati { get; set; }
-        public string? Aciklama { get; set; }
+        public string Aciklama { get; set; }
         public int StokAdedi { get; set; }
     }
 }
